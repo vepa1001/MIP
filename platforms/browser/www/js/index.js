6a06de2ac92d957d9840c098071afbac59925846
t5pave00 <veronicapanteleeva@gmail.com>
2018-02-02 01:31:21 +0200

    First exercise

---
+++ platforms/browser/www/js/index.js
@@ -0,0 +1,34 @@
+
+
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady(){
+
+    document.getElementById('captureBtn').addEventListener('click',takePic);
+    
+
+    function takePic(){
+        navigator.camera.getPicture(cameraSuccess, cameraError, {
+
+            quality: 50,
+            sourceType : Camera.PictureSourceType.CAMERA,
+            destinationType : Camera.DestinationType.FILE_URI
+
+        });
+    }
+    
+    function cameraSuccess(imageData){
+            var img = document.getElementById('myImage');
+            img.src ="data:image/jpeg;base64"+imageData;
+            
+    }
+
+    function cameraError(message) {
+            alert('Error: ' + message);
+
+    }
+
+}
+
+
+ 
